311d5270115de2e2cdb27fcfd3d9f7bfe41c7f21
Olivier Huber <oli.huber@gmail.com>
2020-05-17 19:51:24 +0200

    let go of MacOs problems

--- test/runtests_moi.jl
+++ test/runtests_moi.jl
@@ -10,8 +10,12 @@ using Test
 
 include("MOI/solvers.jl")
 include("MOI/wrapper.jl")
-GC.gc()
-
-include("MOI/solvers_fragile.jl")
-include("MOI/wrapper.jl")
 include("MOI/minlptests.jl")
+
+# GAMS is really fragile on macos and windows
+if Sys.islinux()
+  GC.gc()
+
+  include("MOI/solvers_fragile.jl")
+  include("MOI/wrapper.jl")
+end
